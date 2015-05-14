598bf1408d061f72f1a8c883e5db55b6a1508244
Manuele Menozzi <manuele.menozzi@gmail.com>
2015-05-14 14:24:38 +0200

    Aggiunge RoutingAutoBundle alle dipendenze e configura

--- src/DependencyInjection/WebgriffeCmfPageExtension.php
+++ src/DependencyInjection/WebgriffeCmfPageExtension.php
@@ -4,15 +4,18 @@ namespace Webgriffe\Cmf\PageBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Exception\InvalidArgumentException;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\Yaml\Parser;
 
 /**
  * This is the class that loads and manages your bundle configuration
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class WebgriffeCmfPageExtension extends Extension
+class WebgriffeCmfPageExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritdoc}
@@ -24,5 +27,41 @@ class WebgriffeCmfPageExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+    }
+
+    /**
+     * Allow an extension to prepend the extension configurations.
+     *
+     * @param ContainerBuilder $container
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        /**
+            chain:
+                routers_by_id:
+                    cmf_routing.dynamic_router: 20
+                    router.default: 100
+         */
+        $configs = $this->loadYmlConfig('prepended_config.yml');
+        foreach ($configs as $name => $config) {
+            $container->prependExtensionConfig($name, $config);
+        }
+    }
+
+    protected function loadYmlConfig($file)
+    {
+        $fileLocator = new FileLocator(__DIR__.'/../Resources/config');
+        $file = $fileLocator->locate($file);
+
+        if (!stream_is_local($file)) {
+            throw new InvalidArgumentException(sprintf('This is not a local file "%s".', $file));
+        }
+
+        if (!file_exists($file)) {
+            throw new InvalidArgumentException(sprintf('The service file "%s" is not valid.', $file));
+        }
+
+        $ymlParser = new Parser();
+        return $ymlParser->parse(file_get_contents($file));
     }
 }
