b0376f0f6fa5ea109183be719364aee52a72fc92
Manuele Menozzi <manuele.menozzi@gmail.com>
2015-07-09 08:39:24 +0200

    Adds test.db.dist to fix a wierd problem with phpcr init

--- bin/init_phpcr.sh
+++ bin/init_phpcr.sh
@@ -3,6 +3,7 @@
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 ROOT_DIR=$(dirname "${SCRIPT_DIR}")
 
+cp ${ROOT_DIR}/Tests/Resources/app/test.db.dist ${ROOT_DIR}/Tests/Resources/app/test.db
 php ${ROOT_DIR}/vendor/symfony-cmf/testing/bin/console doctrine:phpcr:init:dbal --drop
 php ${ROOT_DIR}/vendor/symfony-cmf/testing/bin/console cache:clear
 php ${ROOT_DIR}/vendor/symfony-cmf/testing/bin/console doctrine:phpcr:repository:init
