ea580467381c62501775bbc9713b29e2f0a8c99e
Manuele Menozzi <manuele.menozzi@gmail.com>
2015-08-04 09:00:48 +0200

    Enables Menu management from dashboard block

---
+++ Block/MenuBlockService.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: manuele
+ * Date: 03/08/15
+ * Time: 22:21
+ */
+
+namespace PUGX\Cmf\PageBundle\Block;
+
+
+use Sonata\BlockBundle\Block\BaseBlockService;
+use Sonata\BlockBundle\Block\BlockContextInterface;
+use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+use Symfony\Cmf\Bundle\MenuBundle\Provider\PhpcrMenuProvider;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class MenuBlockService extends BaseBlockService
+{
+    /**
+     * @var PhpcrMenuProvider
+     */
+    private $menuProvider;
+
+    /**
+     * @param string $name
+     * @param EngineInterface $templating
+     * @param PhpcrMenuProvider $menuProvider
+     */
+    public function __construct($name, EngineInterface $templating, PhpcrMenuProvider $menuProvider)
+    {
+        parent::__construct($name, $templating);
+        $this->menuProvider = $menuProvider;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function execute(BlockContextInterface $blockContext, Response $response = null)
+    {
+        $settings = array(
+            'id' => $this->menuProvider->getMenuRoot(),
+            'selected' => '',
+        );
+        $settings = array_merge($blockContext->getSettings(), $settings);
+        return $this->renderResponse(
+            $blockContext->getTemplate(),
+            array(
+                'block_context' => $blockContext,
+                'block' => $blockContext->getBlock(),
+                'settings' => $settings,
+            ),
+            $response
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultSettings(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(
+            array(
+                'url' => false,
+                'title' => 'dashboard.menu',
+                'label_catalogue' => 'PUGXCmfPageBundle',
+                'template' => 'PUGXCmfPageBundle:Block:menu.html.twig',
+            )
+        );
+    }
+}
--- Tests/WebTest/AdminDashboardTest.php
+++ Tests/WebTest/AdminDashboardTest.php
@@ -18,9 +18,14 @@ class AdminDashboardTest extends IsolatedTestCase
         $client = static::createClient();
         $crawler = $client->request('GET', '/admin/dashboard');
         $this->assertTrue($client->getResponse()->isSuccessful());
-        $cmsBox = $crawler->filter('.right-side .cms-block');
+        $cmsBox = $crawler->filter('.right-side .content .col-md-6')->eq(0)->filter('.cms-block');
         $this->assertEquals('Content Management System', $cmsBox->filter('.box-header h3')->text());
         $this->assertCount(1, $cmsBox->filter('.box-body table tr'));
         $this->assertContains('Page', $cmsBox->filter('.box-body table tr')->eq(0)->filter('td')->eq(0)->text());
+
+        $menuBox = $crawler->filter('.right-side .content .col-md-6')->eq(1)->filter('.cms-block');
+        $this->assertEquals('Site Menu', $menuBox->filter('.box-header h3')->text());
+        $this->assertContains('"rootNode": "/cms/menu"', $menuBox->filter('.box-body script')->eq(6)->html());
+        $this->assertContains('"selected": "/cms/menu"', $menuBox->filter('.box-body script')->eq(6)->html());
     }
 }
